a11e8ab08f56cada47915ebd6fd415c1863ca6af
Belcescu Cristina <cristina_b963@yahoo.com>
2020-06-11 10:24:53 +0300

    Add files via upload

---
+++ Evolutia proiectului/js/refs.js
@@ -0,0 +1,11 @@
+
+let refs = {
+	
+  'Outline of games': `
+  <dd property="schema:citation" typeof="schema:WebPage"
+      resource="https://www.w3.org/TR/rdfa-primer/">
+    <cite property="schema:name"><a href="https://www.w3.org/TR/rdfa-primer/">Outline of games From Wikipedia</a></cite>.
+  </dd>
+  `,
+};
+export default refs;
